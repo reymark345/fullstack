8e72ab9a8e18f27e3ddb18af0fb8bccebd69478f
reymark <reymarkvaldehueza@gmail.com>
2021-04-16 08:47:44 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+require('./bootstrap');
+
+window.Vue = require('vue')
+
+Vue.component('mainapp', require('./components/mainapp.vue').default)
+const app = new Vue({
+    el:'#app'
+})
---
+++ resources/js/components/mainapp.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+      <h1>fasfasf</h1>
+
+    </div>
+</template>
+
+
