8e7ab8c6ca98e4f300d60467dcc3b2f1d91f0d8f
reymark <reymarkvaldehueza@gmail.com>
2021-04-18 21:12:29 +0800

    develop: Hooks done

--- public/js/app.js
+++ public/js/app.js
@@ -19564,7 +19564,7 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", [_c("h1", [_vm._v("Second page ")])])
+    return _c("div", [_c("h1", [_vm._v("Second page  ")])])
   }
 ]
 render._withStripped = true
